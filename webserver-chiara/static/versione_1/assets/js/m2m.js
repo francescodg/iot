9cfd4ff4a7f9b26f1e90e7c0938af923c8995ecd
francescodg4 <francescodg4@gmail.com>
2017-03-25 13:30:44 +0100

    Added list of temperature sensors

--- webserver-chiara/static/versione_1/assets/js/m2m.js
+++ webserver-chiara/static/versione_1/assets/js/m2m.js
@@ -2,3 +2,20 @@
 function getAverageTemperature(callback) {
     $.get('/temperature/average', callback);
 }
+
+function getTemperatureReadings(callback) {
+    var readings = Array()
+    for (var i = 0; i < 10; i++)
+	readings[i] = Math.random() * 100
+    callback(readings)
+}
+
+var app = angular.module('GreenHouseApp', []);
+
+app.controller('GreenHouseController', function($scope) {
+    $scope.hello = "Hello world, my dear";
+
+    (function init() {
+	getTemperatureReadings(function(readings) { $scope.readings = readings })
+    })();	
+});
