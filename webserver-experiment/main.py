dfdc68469dcb99564f74532958ccc711c2e09fe5
francescodg4 <francescodg4@gmail.com>
2017-03-28 13:03:28 +0200

    View integration with AngulaJS

--- webserver-experiment/main.py
+++ webserver-experiment/main.py
@@ -2,14 +2,48 @@ from flask import Flask, request
 from flask_socketio import SocketIO
 import random
 
+import threading
+
 app = Flask(__name__)
 socketio = SocketIO(app)
-	
+
+class System:
+    def __init__(self):
+        self.temperatureSensors = [1, 2, 3]
+
+    def randomUpdate(self, index):
+        self.temperatureSensors[index] = random.randint(0, 100)
+
+    @property
+    def averageTemperature(self):
+        return sum(self.temperatureSensors) / len(self.temperatureSensors)
+        
+
+system = System()
+
+def timer():
+    # Update system
+    index = random.randint(0, len(system.temperatureSensors)-1)
+    system.randomUpdate(index)
+
+    data = {
+        'id': index,
+        'value': system.temperatureSensors[index]
+    }
+
+    socketio.emit("new temperature", data)
+    socketio.emit("new average temperature", system.averageTemperature)
+
+    threading.Timer(5, timer).start()
+
 @app.route("/send")
 def send():
-        print("Request on send")
-        socketio.emit('new data', {'data': str(random.randint(0, 10))})
-        return "Send"
+    print("Request on send")
+    socketio.emit('new data', {'data': str(random.randint(0, 10))})
+    return "Send"
 
 if __name__ == "__main__":
+    timer()
     socketio.run(app, debug=True)
+
+
--- webserver-experiment/static/websocket.js
+++ webserver-experiment/static/websocket.js
@@ -1,3 +1,4 @@
+var $_scope;
 
 $(document).ready(function(){		
     var socket = io.connect("http://127.0.0.1:5000");
@@ -6,9 +7,33 @@ $(document).ready(function(){
 	console.log(value)
 	// $('#subscribeTxt').val(logStr)
     });
+
+    socket.on('new temperature', function(data) {
+    	var i = data.id;
+    	var value = data.value;	
+    	$_scope.system.temperatureSensors[i] = value;    	
+    	$_scope.$apply();
+    });
+
+    socket.on('new average temperature', function(data) {
+    	console.log(data)
+	
+    	$_scope.system.averageTemperature = data
+    	
+    	$_scope.$apply();
+    });
 		
 
     $('#startBtn').click(function() {
 	$.get('http://127.0.0.1:5000/send')
     });
 });
+
+app = angular.module("app", [])
+app.controller("appCtrl", function($scope) {
+    $_scope = $scope
+    $_scope.system = {
+    	temperatureSensors: new Array(3),
+	averageTemperature: null
+    }
+})
